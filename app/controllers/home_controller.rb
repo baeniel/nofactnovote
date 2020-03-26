cf445573eded1d193b8b5dbfb0bf5c31c928127c
baeniel <jinveloper126@gmail.com>
2020-03-26 21:42:45 +0900

    지도 삭제

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @data = JSON.parse(File.read("#{Rails.root}/public/korea.json"))
-
-    #data의 위도들만 담아내기 위해서
-    @positions = []
-
-    #그 위도에 해당하는 지역구를 담아내기 위해서
-    @districts = []
-
-    @count = 0
-
-    while @count < @data["positions"].length
-      @positions << @data["positions"][@count]["lat"]
-      @districts << District.find_by(lat: @positions[@count])
-
-      @count = @count + 1
-    end
+    
   end
 end
