898befbc4304cf688485273a5090093e964124f8
baeniel <jinveloper126@gmail.com>
2020-04-05 12:12:54 +0900

    admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
 
-  permit_params :password, :password_confirmation, :party, :name, :district_id, :image, :policy
+  permit_params :password, :password_confirmation, :party, :name, :district_id, :image, :policy, :link1, :link2, :link3, :lin4, :head1, :head2, :head3, :head4
 
   index do
     selectable_column
@@ -16,8 +16,6 @@ ActiveAdmin.register User do
     end
     column :visit
     column :link1
-    column :link2
-    column :link3
     actions
   end
 
@@ -39,6 +37,14 @@ ActiveAdmin.register User do
       f.input :image
       f.input :password
       f.input :password_confirmation
+      f.input :link1
+      f.input :link2
+      f.input :link3
+      f.input :link4
+      f.input :head1
+      f.input :head2
+      f.input :head3
+      f.input :head4
     end
     f.actions
   end
