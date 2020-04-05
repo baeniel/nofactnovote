9231ac97db1532253adf81c9a16e3ccb0fd30a3a
baeniel <jinveloper126@gmail.com>
2020-04-05 11:50:35 +0900

    tmp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     if params[:keyword].present?
       @keyword = params[:keyword].gsub(/\s+/, "")
 
-      @district = District.ransack(name_cont_any: @keyword).result(distinct: true)
+      @district = District.ransack(name_cont: @keyword).result(distinct: true)
       @district = District.find_by(id: @district)
 
       @congress = User.ransack(name_cont_any: @keyword).result(distinct: true)
