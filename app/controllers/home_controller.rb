156319e9ec7afeb16070b5aac29214400a7d44f8
baeniel <jinveloper126@gmail.com>
2020-04-07 18:42:12 +0900

    보완

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,10 +19,10 @@ class HomeController < ApplicationController
       end
     end
 
-    # url = 'http://apis.data.go.kr/9760000/CommonCodeService/getCommonSggCodeList'
-    # headers = { :params => { CGI::escape('ServiceKey') => 'pBCDhUSP1lJ77BU58EJBQUlcKirh4gX1ap427Pya5Jws%2FsBX7jKtz4IwOuB12v0W%2FVN8MM%2B0Yl%2FuOxXnvJFdpA%3D%3D',CGI::escape('ServiceKey') => '-',CGI::escape('pageNo') => '1',CGI::escape('numOfRows') => '10',CGI::escape('sgId') => '20200415',CGI::escape('sgTypecode') => '2' } }
-    # response = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
-    # puts response
+    url = 'http://apis.data.go.kr/9760000/CommonCodeService/getCommonSggCodeList'
+    headers = { :params => { CGI::escape('ServiceKey') => 'pBCDhUSP1lJ77BU58EJBQUlcKirh4gX1ap427Pya5Jws%2FsBX7jKtz4IwOuB12v0W%2FVN8MM%2B0Yl%2FuOxXnvJFdpA%3D%3D',CGI::escape('ServiceKey') => '-',CGI::escape('pageNo') => '1',CGI::escape('numOfRows') => '10',CGI::escape('sgId') => '20200415',CGI::escape('sgTypecode') => '2' } }
+    response = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
+    puts response
 
     # if params[:user_id].present?
     #   @user = User.find(params[:user_id])
